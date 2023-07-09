0dbaa7969efb5f15d62682f13b0e4686ee01e2fe
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-07-09 19:26:51 +0800

    修改了 缩略图 组件的大小

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -3,14 +3,20 @@ import React from "react";
 export default function Thumbnail({
   src,
   title,
+  className,
 }: {
   src: string;
   title: string;
+  className?: string;
 }) {
   return (
     <>
       {src ? (
-        <div className="relative w-80 justify-center flex flex-col ">
+        <div
+          className={`relative w-80 justify-center flex flex-col ${
+            className ? className : ""
+          } `}
+        >
           {title && (
             <div className="absolute top-0 w-full text-center bg-black/50">
               {title}
